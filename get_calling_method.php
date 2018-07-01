07589878edad538beede82b8fa3b0db3e4b3f494
Andrey Kaplunenko <me@kaplunenko.name>
2018-07-01 16:04:04 +0300

    added wrapping class around

--- get_calling_method.php
+++ get_calling_method.php
@@ -5,12 +5,13 @@
  * Date: 7/1/18
  * Time: 10:31 AM
  */
-function getCallingMethod(){
-    $exc = new Exception();
-    $trace = $exc->getTrace();
-    //position 0 would be the line that called this function so we ignore it
-    //$last_call = $trace[1]; //uncomment this and comment next line if we want max info
-    $last_call = 'Executed method: '.$trace[1]['class'].'->'.$trace[1]['function'];
-    print_r($last_call, false);
-    print_r(PHP_EOL);
+class ExampleClass {
+    protected function getCallingMethod(){
+        $exc = new Exception();
+        $trace = $exc->getTrace();
+        //position 0 would be the line that called this function so we ignore it
+        //$last_call = $trace[1]; //uncomment this and comment next line if we want max info
+        $last_call = 'Executed method: '.$trace[1]['class'].'->'.$trace[1]['function'];
+        print_r($last_call, false);
+    }
 }
